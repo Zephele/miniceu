c62973c0af41f6e40adb380ca688be208fa10e6f
Pedro Henrique De Jesus Barbosa <pede-jes@c2r2p1.42sp.org.br>
2025-08-26 11:59:29 -0300

    ok

--- src/tokens/aux3_tokens.c
+++ src/tokens/aux3_tokens.c
@@ -6,7 +6,7 @@
 /*   By: pede-jes <pede-jes@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 21:01:22 by ratanaka          #+#    #+#             */
-/*   Updated: 2025/08/25 19:57:00 by pede-jes         ###   ########.fr       */
+/*   Updated: 2025/08/26 11:58:11 by pede-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,40 +60,4 @@ char	*aux_general(char *input, int *i, char *content)
 	return (content);
 }
 
-static int	handle_dollar_case(char *input, int *i, char quote, char **content)
-{
-	int		start;
-	char	*temp;
 
-	start = *i;
-	while (input[*i] && input[*i] != quote)
-		(*i)++;
-	temp = ft_substr(input, start, *i - start);
-	if (is_empty(temp, *content))
-		return (0);
-	*content = ft_strjoin_free(*content, temp);
-	if (quote == '"' || temp[0] == '"')
-		*content = expand_env_vars(*content);
-	free(temp);
-	return (1);
-}
-
-char	*extract_quoted_content(char *input, int *i, char quote)
-{
-	char	*content;
-	int		status;
-
-	content = ft_strdup("");
-	while (condition(input, *i, quote))
-	{
-		if (input[*i] == quote)
-			status = handle_quote_case(input, i, quote, &content);
-		else
-			status = handle_dollar_case(input, i, quote, &content);
-		if (status == 0)
-			return (NULL);
-		if (status == 2)
-			break ;
-	}
-	return (content);
-}
